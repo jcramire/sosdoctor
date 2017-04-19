203f0fd05d50b9403e7f13735ad544836a42a56c
jcramire <jcramire1992@gmail.com>
2017-04-19 15:18:43 -0300

    first-commit

---
+++ sosdoctor/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-firebase.FirebasePlugin",
+        "file": "plugins/cordova-plugin-firebase/www/firebase.js",
+        "pluginId": "cordova-plugin-firebase",
+        "clobbers": [
+            "FirebasePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.5",
+    "cordova-plugin-firebase": "0.1.20",
+    "cordova-plugin-whitelist": "1.3.2"
+};
+// BOTTOM OF METADATA
+});
