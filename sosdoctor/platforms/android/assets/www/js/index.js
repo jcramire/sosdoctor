ffe52a321468e71abbc1a78935bcc211b6747af3
jcramire <jcramire1992@gmail.com>
2017-04-20 01:01:09 -0300

    primer avance

--- sosdoctor/platforms/android/assets/www/js/index.js
+++ sosdoctor/platforms/android/assets/www/js/index.js
@@ -9,13 +9,221 @@ var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 });
+
 document.addEventListener('deviceready', this.onDeviceReady, false);
-function onDeviceReady(){
-    console.log('device'+device.platform+'uuid'+device.uuid);
-    window.FirebasePlugin.getToken(function(token) {
-    // save this server-side and use it to push notifications to this device
-        alert(token);
-    }, function(error) {
-        console.error(error);
-    });
+
+function onDeviceReady() {
+  // TEST
+  // console.log('device'+device.platform+'uuid'+device.uuid);
+  // window.FirebasePlugin.getToken(function(token) {
+  //     console.log("token es" + token);
+  // }, function(error) {
+  //     console.error(error);
+  // });
+
 }
+
+/*-----------------------------------------------------------------------------*/
+/*--------------------------------- Index -------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+if (localStorage.getItem('login')=="on") {
+    mainView.router.loadPage('home.html');
+}
+$$(".close-panel").on('click',function () {
+    localStorage.setItem('login',"off");
+    //mainView.router.reloadPage('index.html');
+});
+
+/* Login */
+$$('.login').on('click', function() {
+  var bol_username, bol_password;
+  var username = $$('input[name="username"]').val();
+  var password = $$('input[name="password"]').val();
+  if (!username || !password) {
+    myApp.alert('Debe ingresar usuario y contraseña', 'Atención!');
+  } else {
+    /* validar el ingreso de los parametros de acceso */
+    if (bol_username = validarUsername(username)) {
+      if (bol_password = validarPassword(password)) {
+        var parametros = new Object();
+
+        /* platform android */
+        //parametros.email = username;
+        //parametros.password = password;
+        //parametros.dispositivo = device.platform;
+        //parametros.imei = device.uuid;
+        //parametros.fcm = localStorage.getItem("registrationFCM");
+
+        /* platform desktop */
+        parametros.email = username;
+        parametros.password = password;
+        parametros.dispositivo = 'android';
+        parametros.imei = 'cd83d9d3583c9360';
+        parametros.fcm = 'enZ6ii9dESE:APA91bGoU_NyGqEQQAeF62WyzpKjZ5MDeWrpQVH7axVNjVfAv_K17C1oxWCV8gETjracT7zrGNU6pAjqd2vKNMLl9vt_Roo4viVecKwpa4dVzYJuJ-zdaWXmqhvy4pE6rrBa9J59c3Ea';
+        console.log(JSON.stringify(parametros));
+        $$.ajax({
+          type: "POST",
+          dataType: "jsonp",
+          data: parametros,
+          url: "http://190.98.210.37/jcr/webservices/login.php",
+          success: function(response) {
+            var objeto = JSON.parse(response);
+            if (objeto.error == false) {
+              //myApp.closeModal('.login-screen');
+              mainView.router.loadPage('home.html');
+              localStorage.setItem('login', "on");
+              localStorage.setItem("id_doctor", objeto.user.id_doctor);
+              localStorage.setItem("nombre", objeto.user.nombre);
+              localStorage.setItem("apellido", objeto.user.apellido);
+            } else {
+              myApp.alert(objeto.error_msg, 'Atención!');
+            }
+          },
+          error: function(response) {
+            myApp.hidePreloader();
+            myApp.alert("Error de conexion", 'Error!');
+          }
+        });
+      }
+    }
+  }
+});
+/* Recover*/
+$$('.recover').on('click', function() {
+  myApp.prompt('Ingrese su correo para enviar una nueva contraseña', 'Recuperar contraseña', function(value) {
+    if (validarUsername(value)) {
+      myApp.confirm('Pedir nueva contraseña?', 'Confirmar', function() {
+        var parametros = new Object();
+        parametros.email = value;
+        $$.ajax({
+          type: "POST",
+          dataType: "jsonp",
+          data: parametros,
+          url: "http://190.98.210.37/jcr/webservices/recover.php",
+          success: function(response) {
+            console.log(response);
+            var objeto = JSON.parse(response);
+            if (objeto.error == false) {
+              myApp.alert(objeto.success_msg, 'Atención!');
+            } else {
+              myApp.alert(objeto.error_msg, 'Atención!');
+            }
+          },
+          error: function(response) {
+            myApp.alert("Error de conexion", 'Error!');
+          }
+        });
+
+      });
+    }
+  });
+  /*personaliza prompt*/
+  $$(".modal-text-input").attr('placeholder', 'correo@dominio.cl');
+  $$(".theme-deeporange .modal-button").css('color', '#4caf50');
+});
+
+/* validar parametros de Login [username(correo), password] */
+function validarUsername(username) {
+  expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  if (!expr.test(username)) {
+    myApp.hidePreloader();
+    myApp.alert('El usuario es invalido', 'Atención!');
+    return false;
+  }
+  return true;
+}
+
+function validarPassword(password) {
+  if (password.length == 0) {
+    myApp.alert('La contraseña es obligatoria', 'Atención!');
+    return false;
+  }
+  expr = /^[a-zA-Z_ÁÉÍÓÚáéíóúñÑ\s\d]*$/;
+  if (!expr.test(password)) {
+    myApp.alert('La contraseña es invalida <br> solo: letras y numeros', 'Atención!');
+    return false;
+  }
+  return true;
+}
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+
+
+
+/*-----------------------------------------------------------------------------*/
+/*---------------------------------- Home -------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+myApp.onPageInit('home', function(page) {
+  // Cargar librerias
+  $$("#maps").attr("src", "js/maps.js");
+  $$("#mapslibrary").attr("src", "https://maps.googleapis.com/maps/api/js?key=AIzaSyAnsA4AvAJtzqY-qUyJxXLWQSOM3w2FAcI&callback=initMap");
+  $$("#avatar_doctor").attr("src", "img/avatar_doctor/"+localStorage.getItem("id_doctor")+".png");
+  $$(".subnavbar .buttons-row .center").append(
+    'Dr. ' + localStorage.getItem("nombre") + ' ' + localStorage.getItem("apellido") + ' <br>Pacientes Asignados'
+  );
+})
+
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+
+
+
+/*-----------------------------------------------------------------------------*/
+/*------------------------------- Pacientes -----------------------------------*/
+/*-----------------------------------------------------------------------------*/
+/* Listado */
+myApp.onPageInit('listado', function(page) {
+  $$("#avatar_doctor").attr("src", "img/avatar_doctor/"+localStorage.getItem("id_doctor")+".png");
+  $$(".subnavbar .buttons-row .center").append(
+    'Dr. ' + localStorage.getItem("nombre") + ' ' + localStorage.getItem("apellido") + ' <br>Pacientes Asignados'
+  );
+
+
+  /*Cargar listado*/
+  var parametros = new Object();
+  parametros.id_doctor = localStorage.getItem("id_doctor");
+  $$.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    data: parametros,
+    url: "http://190.98.210.37/jcr/webservices/pacientes_listado.php",
+    success: function(response) {
+      var objeto = JSON.parse(response);
+      if (objeto.error == false) {
+        var objeto = JSON.parse(response);
+        localStorage.setItem("pacientes", JSON.stringify(objeto.pacientes));
+        cargarPacientes();
+      } else {
+        myApp.alert(objeto.error_msg, 'Atención!');
+      }
+    },
+    error: function(response) {
+      myApp.hidePreloader();
+      myApp.alert("Error de conexion", 'Error!');
+    }
+  });
+
+  function cargarPacientes() {
+    var pacientes = localStorage.getItem("pacientes");
+    $$(".list-block-pacientes ul").html("");
+    pacientes = JSON.parse(pacientes);
+    for (var i = 0; i < pacientes.length; i++) {
+      $$(".list-block-pacientes ul").append(
+        '<li class="item-content" id="' + pacientes[i].id_paciente + '" onclick="paciente(this.id)">' +
+        '<div class="item-media"><img src="img/avatar_paciente/' + pacientes[i].id_imagen + '.png" height="40" width="40"></div>' +
+        '<div class="item-inner">' +
+        '<div class="item-title"><strong>' + pacientes[i].nombre + " - " + pacientes[i].apellido + '</strong></div>' +
+        '<div class="item-title">' + pacientes[i].direccion + " - " + pacientes[i].comuna + '</div>' +
+        '<div class="item-title" style="color: red">' + pacientes[i].fecha_revision + '</div>' +
+        '</div>' +
+        '<div class="item-media"><img src="img/listado_paciente/mapa_paciente.svg" height="29" width="29"></div>' +
+        '</li>'
+      );
+    }
+  }
+})
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
---
+++ sosdoctor/www/js/maps.js
@@ -0,0 +1,203 @@
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -33.398981,
+      lng: -70.557312
+    },
+    zoom: 13,
+    scrollwheel: false,
+    zoomControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+  });
+  //definición de variables
+  var listaprincipal;
+  localStorage.setItem("pacientes", "[]");
+  var markersArray = [];
+
+  var parametros = new Object();
+  parametros.id_doctor = localStorage.getItem("id_doctor");
+  $$.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    data: parametros,
+    url: "http://190.98.210.37/jcr/webservices/pacientes_listado.php",
+    success: function(response) {
+      var objeto = JSON.parse(response);
+      if (objeto.error == false) {
+        //define listaprincipal y recibe listaresponse
+        var listaresponse = objeto.pacientes;
+        listaprincipal = JSON.parse(localStorage.getItem("pacientes"));
+        console.log("tamaño de listaprincipal " + listaprincipal.length);
+        console.log("tamaño de listaresponse " + listaresponse.length);
+        console.log("tamaño de markersArray " + markersArray.length);
+        if (markersArray.length > 0) {
+          //si no viene del listaresponse se elimina de listaprincipal
+          for (var j = 0; j < listaprincipal.length; j++) {
+            var camP = listaprincipal[j];
+            var viene = 0;
+            for (var i = 0; i < listaresponse.length; i++) {
+              var camR = listaresponse[i];
+              if (parseInt(camR.id_paciente) == parseInt(camP.id_paciente) && parseInt(camR.id_estado) != parseInt(camP.id_estado)) {
+                viene = 0;
+                break;
+              }
+              if (parseInt(camR.id_paciente) != parseInt(camP.id_paciente)) {
+                viene = 0;
+              } else {
+                if (parseInt(camR.id_paciente) == parseInt(camP.id_paciente)) {
+                  viene = 1;
+                  break;
+                }
+
+              }
+
+            }
+            console.log("Viene " + viene);
+            //console.log("Ids "+ camR.id+camP.id+" and "+camR.id_estado+" "+camP.id_estado);
+            if (viene == 0) {
+              console.log("eliminar");
+              removeMarker(camP.id);
+              listaprincipal.splice(j, 1);
+              j--;
+            }
+          }
+          // elimina de listaresponse el objeto existe listaprincipal para optimizar busqueda
+
+          for (var j = 0; j < listaprincipal.length; j++) {
+            var viene = false;
+            for (var i = 0; i < listaresponse.length; i++) {
+              var camR = listaresponse[i];
+              var camP = listaprincipal[j];
+              if (parseInt(camR.id_paciente) == parseInt(camP.id_paciente) && parseInt(camR.id_estado) == parseInt(camP.id_estado)) {
+                //elimina objeto de listaresponse existente en listaprincipal
+                listaresponse.splice(i, 1);
+                i--;
+                break;
+              }
+            }
+          }
+          console.log("agregar tamaño de listaresponse " + listaresponse.length);
+          //encola listadoresponse a listaprincipal
+          for (var i = 0; i < listaresponse.length; i++) {
+            var largo = listaprincipal.length;
+            listaprincipal[largo] = listaresponse[i];
+            //agrega marcador a mapa
+            console.log("agregar");
+            markera(listaresponse[i]);
+            //nuevas(listaresponse[i]);
+          }
+          //console.log("localStorage: "+localStorage.getItem("camera") );
+          localStorage.setItem("camera", JSON.stringify(listaprincipal));
+        } else {
+          console.log("primera vez");
+          localStorage.setItem("pacientes", JSON.stringify(listaresponse));
+          //agrega cada uno de los marcadores de lista response
+          for (var i = 0; i < listaresponse.length; i++) {
+            var largo = listaprincipal.length;
+            listaprincipal[largo] = listaresponse[i];
+            //llama a la funcion para agregar marcador
+            console.log("agrega");
+            markera(listaresponse[i]);
+            //if(i==0){
+            //nuevas(listaresponse[i]);
+            //}
+            //localStorage.setItem("camera",JSON.stringify(listaprincipal) );
+          }
+        }
+      } else {
+        myApp.alert('alerta alerta', 'Atención!');
+      }
+    },
+    error: function(response) {
+      myApp.hidePreloader();
+      myApp.alert("Error de conexion", 'Error!');
+    }
+  });
+
+
+  function removeMarker(position) {
+    //console.log("markersArray "+ markersArray.length);
+    for (var i = 0; i < markersArray.length; i++) {
+      //console.log("position array "+markersArray[i].id);
+      if (markersArray[i].id == position) {
+        console.log("delete " + markersArray[i].id + "== " + position);
+        markersArray[i].setMap(null);
+        markersArray.splice(i, 1);
+        i--;
+      }
+
+    }
+
+    //console.log(markersArray.length);
+  }
+
+  function markera(pacientes) {
+    var iconoMarca;
+    //tipo de camara publica
+    console.log(pacientes.id_estado);
+    if (parseInt(pacientes.id_estado) == 0) {
+      iconoMarca = 'img/marcador/marcador_gris.svg';
+    }
+    if (parseInt(pacientes.id_estado) == 1) {
+      iconoMarca = 'img/marcador/marcador_camara.svg';
+    }
+    if (parseInt(pacientes.id_estado) == 2) {
+      iconoMarca = 'img/marcador/marcador_camara_alerta.svg';
+    }
+    //marcador
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(pacientes.latitud, pacientes.longitud),
+      animation: google.maps.Animation.DROP,
+      icon: iconoMarca,
+      optimized: false,
+      id: pacientes.id_paciente,
+    });
+    //setea marcador en mapa
+    marker.setMap(map);
+    markersArray.push(marker);
+    //card paciente
+    marker.addListener('click', function() {
+      var modal = myApp.modal({
+        afterText: '<div class="swiper-container" id="paciente-modal" style="width: auto; margin:5px -15px -15px">' +
+          '<div class="img-paciente">' +
+          '<div class="swiper-slide"><img src="img/modal_paciente/paciente_tam_ori.jpg" height="150" style="display:block"></div>' +
+          '<div class="text-paciente">' +
+          '<div>Paciente</div>' +
+          '<div>' + pacientes.nombre + '</div>' +
+          '</div>' +
+          '</div>' +
+          '<div class="info-paciente">' +
+          '<div class="detalle">' +
+          '<img src="img/modal_paciente/encama.svg"></img>' +
+          '<div>' +
+          '<div>Movilidad: ' + pacientes.tipo + '</div>' +
+          '<div>' + pacientes.enfermedad_cronica + '</div>' +
+          '</div>' +
+          '</div>' +
+          '<div class="control">' +
+          '<img src="img/modal_paciente/pide_hora.svg"></img>' +
+          '<div>' +
+          '<div>Ultimo control online: ' + pacientes.fecha_revision + '</div>' +
+          '</div>' +
+          '</div>' +
+          '</div>' +
+          '</div>',
+        buttons: [
+          {
+            text: 'Detalle paciente',
+            bold: true,
+            onClick: function() {
+              mainView.router.loadPage('listado_paciente.html');
+            }
+          },
+        ]
+      })
+    });
+  }
+}
